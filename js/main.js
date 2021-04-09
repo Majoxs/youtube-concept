555dc799f87f5bc77b4b2dcede1b14c6f37b4742
Majoxs <56292597+Majoxs@users.noreply.github.com>
2021-04-09 13:44:20 +0500

    Пофиксили меню и шапку, добавили контент.

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+
+});
+
+const swiperRecommended = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+
+});
+
+const food = new Swiper('.food-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.food-button-next',
+    prevEl: '.food-button-prev',
+  },
+
+});
