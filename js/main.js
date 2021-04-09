41afad2b1898347d8c85b70ec29892bebfb878a7
Majoxs <56292597+Majoxs@users.noreply.github.com>
2021-04-09 17:14:16 +0500

    Добавили правила для адаптива

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-const swiper = new Swiper('.channel-slider', {
+const myChannelSlider = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    }
+  },
 
   // Navigation arrows
   navigation: {
@@ -11,10 +29,19 @@ const swiper = new Swiper('.channel-slider', {
 
 });
 
-const swiperRecommended = new Swiper('.recommended-slider', {
+const recommendedSlider = new Swiper('.recommended-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 2,
+    },
+  },
 
   // Navigation arrows
   navigation: {
@@ -24,10 +51,28 @@ const swiperRecommended = new Swiper('.recommended-slider', {
 
 });
 
-const food = new Swiper('.food-slider', {
+const foodSlider = new Swiper('.food-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    }
+  },
 
   // Navigation arrows
   navigation: {
@@ -36,3 +81,15 @@ const food = new Swiper('.food-slider', {
   },
 
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  myChannelSlider.destroy();
+  recommendedSlider.destroy();
+  foodSlider.destroy();
+}
